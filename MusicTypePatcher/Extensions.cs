8353ada8d91758df747c0e1fcca572177e382d02
aglowinthefield <tom.auty+nexus@gmail.com>
2024-08-09 15:10:58 -0400

    init

---
+++ MusicTypePatcher/Extensions.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MusicTypePatcher
+{
+    internal static class Extensions
+    {
+        public static IEnumerable<T> Intersection<T>(this IEnumerable<T>? source, IEnumerable<T>? other, IEqualityComparer<T>? comparer = null)
+        {
+            var _comparer = comparer ?? EqualityComparer<T>.Default;
+
+            if (source == null || other == null)
+                yield break;
+
+            var set = new HashSet<T>(source, _comparer);
+            var lookup = other.ToLookup(u => u, _comparer);
+
+            if (set.Count is 0 || lookup.Count is 0)
+            {
+                yield break;
+            }
+
+            foreach (var it in set)
+            {
+                var lookupCount = lookup[it].Count();
+                var sourceCount = source.Count(u => _comparer.Equals(u, it));
+
+                foreach (var item in Enumerable.Repeat(it, Math.Min(lookupCount, sourceCount)))
+                {
+                    yield return item;
+                }
+            }
+        }
+
+        public static IEnumerable<T> DisjunctLeft<T>(this IEnumerable<T> left, IEnumerable<T>? right, IEqualityComparer<T>? comparer = null) where T : notnull
+        {
+            _ = left ?? throw new ArgumentNullException(nameof(left));
+
+            if (right == null || !right.Any())
+            {
+                foreach (var it in left)
+                    yield return it;
+                yield break;
+            }
+
+            var dict = new Dictionary<T, int>(comparer);
+
+            foreach (var it in right)
+            {
+                if (!dict.TryAdd(it, 1))
+                    dict[it]++;
+            }
+
+            foreach (var it in left)
+            {
+                if (dict.TryGetValue(it, out int count) && count > 0)
+                {
+                    dict[it] = count - 1;
+                    continue;
+                }
+
+                yield return it;
+            }
+        }
+    }
+}
---
+++ MusicTypePatcher/Patcher.cs
@@ -0,0 +1,78 @@
+﻿using Mutagen.Bethesda;
+using Mutagen.Bethesda.Skyrim;
+using Mutagen.Bethesda.Synthesis;
+using Noggog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Mutagen.Bethesda.Plugins;
+using Mutagen.Bethesda.Plugins.Cache;
+using Mutagen.Bethesda.Plugins.Records;
+
+namespace MusicTypePatcher
+{
+    public class Patcher
+    {
+        private static uint Timestamp { get; } = (uint)(Math.Max(1, DateTime.Today.Year - 2000) << 9 | DateTime.Today.Month << 5 | DateTime.Today.Day);
+
+        public static async Task<int> Main(string[] args)
+        {
+            return await SynthesisPipeline.Instance
+                .SetTypicalOpen(GameRelease.SkyrimSE, "MusicTypes Merged.esp")
+                .AddPatch<ISkyrimMod, ISkyrimModGetter>(Apply)
+                .Run(args);
+        }
+
+        private static IEnumerable<IModContext<TGet>> ExtentContexts<TGet>(IPatcherState<ISkyrimMod, ISkyrimModGetter> state, FormKey formKey)
+            where TGet : class, IMajorRecordGetter
+        {
+            // Get every context for this musicType
+            var contexts = state.LinkCache.ResolveAllSimpleContexts<TGet>(formKey).ToArray();
+            var masters = contexts.SelectMany(i => state.LoadOrder.TryGetValue(i.ModKey)?.Mod?.MasterReferences ?? new List<IMasterReferenceGetter>(), (i, g) => g.Master)
+                .ToHashSet();
+
+            foreach (var ctx in contexts)
+            {
+                if (!masters.Contains(ctx.ModKey))
+                    yield return ctx;
+            }
+        }
+
+        private static void Apply(IPatcherState<ISkyrimMod, ISkyrimModGetter> state)
+        {
+            using var loadOrder = state.LoadOrder;
+
+            foreach (var musicType in loadOrder.PriorityOrder.OnlyEnabled().MusicType().WinningOverrides())
+            {
+                Console.WriteLine("Processing MusicType {0}", musicType);
+                ProcessMusicType(state, musicType);
+            }
+        }
+
+        private static void ProcessMusicType(IPatcherState<ISkyrimMod, ISkyrimModGetter> state, IMusicTypeGetter musicType)
+        {
+            var origin = state.LinkCache.Resolve<IMusicTypeGetter>(musicType.FormKey);
+            var extentContexts = ExtentContexts<IMusicTypeGetter>(state, musicType.FormKey).ToList();
+            if (extentContexts.Count < 2)
+            {
+                return;
+            }
+
+            var copy = state.PatchMod.MusicTypes.GetOrAddAsOverride(origin);
+            copy.FormVersion = 44;
+            copy.VersionControl = Timestamp;
+            copy.Tracks = new ExtendedList<IFormLinkGetter<IMusicTrackGetter>>();
+
+            var originalTracks = origin.Tracks.EmptyIfNull();
+            int originalTrackCount = originalTracks.Count();
+
+            var extentTracks = extentContexts.Select(static i => i.Record.Tracks.EmptyIfNull());
+
+            extentTracks.Aggregate(originalTracks, (i, k) => i.Intersection(k)).ForEach(copy.Tracks.Add);
+            copy.Tracks.AddRange(extentTracks.SelectMany(i => i.DisjunctLeft(copy.Tracks)));
+
+            Console.WriteLine("Copied {0} tracks to {1}", copy.Tracks.Count - originalTrackCount, copy.EditorID);
+        }
+    }
+}
